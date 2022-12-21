72e68be9ab456214e4f728ee34c06d6e8bcdb57f
Julia <juliavasconceloscr@gmail.com>
2022-12-20 21:08:30 -0300

    Criando o componente e serviço de contatos

---
+++ schedule-app/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { MatButtonModule } from '@angular/material/button'; 
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ContactComponent } from './contact/contact.component';
+import { ContactService } from './contact.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContactComponent
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatButtonModule,
+
+    BrowserAnimationsModule
+  ],
+  providers: [
+    ContactService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
