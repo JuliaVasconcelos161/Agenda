21b72d78a16c4fffdf786422efa9192ab62b80ff
Julia <juliavasconceloscr@gmail.com>
2022-12-20 21:27:29 -0300

    Formulário de contatos

--- schedule-app/src/app/app.module.ts
+++ schedule-app/src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { MatButtonModule } from '@angular/material/button'; 
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar'; 
+import { MatInputModule } from '@angular/material/input';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -18,6 +21,8 @@ import { ContactService } from './contact.service';
     BrowserModule,
     AppRoutingModule,
     MatButtonModule,
+    MatToolbarModule,
+    MatInputModule,
 
     BrowserAnimationsModule
   ],
