85dd98f19966ff08cda7054baca291afb92c582d
Julia <juliavasconceloscr@gmail.com>
2022-12-21 18:20:17 -0300

    Criando componente de dialog

--- schedule-app/src/app/contact/contact.ts
+++ schedule-app/src/app/contact/contact.ts
@@ -3,6 +3,7 @@ export class Contact {
     name?: string;
     email?: string;
     favorite?: boolean;
+    picture?: any;
 
     constructor( name: string, email:string) {
         this.name = name;
